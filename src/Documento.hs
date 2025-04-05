21cc27df2eaa295cddc5701de8c42d7cff54f8d5
MateDominguez <matedominguez2@gmail.com>
2025-04-05 17:06:01 -0300

    Comentarios Ejercicio 2

--- src/Documento.hs
+++ src/Documento.hs
@@ -28,24 +28,32 @@ texto t | '\n' `elem` t = error "El texto no debe contener saltos de línea"
 texto [] = Vacio
 texto t = Texto t Vacio
 
--- foldDoc :: ... PENDIENTE: Ejercicio 1 ...
+-- Ejercicio 1
 foldDoc :: b -> (String -> b -> b) -> (Int -> b -> b) -> Doc -> b
-foldDoc cVacio cTexto cLinea Vacio = cVacio
-foldDoc cVacio cTexto cLinea (Texto s d) = cTexto s (foldDoc cVacio cTexto cLinea d)
-foldDoc cVacio cTexto cLinea (Linea i d) = cLinea i (foldDoc cVacio cTexto cLinea d)
-
+foldDoc cVacio cTexto cLinea Vacio = cVacio -- Caso base Vacio
+foldDoc cVacio cTexto cLinea (Texto s d) = cTexto s (foldDoc cVacio cTexto cLinea d) -- Caso recursivo Texto
+foldDoc cVacio cTexto cLinea (Linea i d) = cLinea i (foldDoc cVacio cTexto cLinea d) -- Caso recursivo Linea
 
 -- NOTA: Se declara `infixr 6 <+>` para que `d1 <+> d2 <+> d3` sea equivalente a `d1 <+> (d2 <+> d3)`
 -- También permite que expresiones como `texto "a" <+> linea <+> texto "c"` sean válidas sin la necesidad de usar paréntesis.
 infixr 6 <+>
 
+--  Ejercicio 2
 (<+>) :: Doc -> Doc -> Doc
-d1 <+> d2 = foldDoc d2 (\t rec -> case d2 of
-  Texto s dd2 -> case rec of
-    Texto s Vacio -> Texto (t++s) dd2
-    _ -> Texto t rec
-  _ -> Texto t rec ) (\i rec -> Linea i rec ) d1
-
+(<+>) d1 d2 =
+  foldDoc
+    d2 -- Caso Vacio
+    ( \s1 rec ->
+        case d2 of
+          Texto s2 d2' ->
+            -- Si d2 empieza con texto,
+            case rec of
+              Texto s2 Vacio -> Texto (s1 ++ s2) d2' -- y rec es Texto s Vacio: concatena los strings.
+              _ -> Texto s1 rec -- Caso Contrario: Sigue la recursion
+          _ -> Texto s1 rec -- Caso Contrario: Sigue la recursion
+    ) -- Caso Texto
+    (\i rec -> Linea i rec) -- Caso Linea
+    d1
 
 indentar :: Int -> Doc -> Doc
 indentar i = error "PENDIENTE: Ejercicio 3"
